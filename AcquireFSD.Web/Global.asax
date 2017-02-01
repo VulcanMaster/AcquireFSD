c1beaee2fec041c24817cdeb62b8dffb51e93418
Dmitrijs Blinkovs <dmitrijs.blinkovs@atea.com>
2017-02-01 15:57:22 +0200

    Add project files.

---
+++ AcquireFSD.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AcquireFSD.Web.MvcApplication" Language="C#" %>
---
+++ AcquireFSD.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcquireFSD.Web.Startup))]
+namespace AcquireFSD.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
