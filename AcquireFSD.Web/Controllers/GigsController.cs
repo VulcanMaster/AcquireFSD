219cf22af361aa6f8b509ee84f28687281c5c151
VulcanMaster <dmitry.blinkov@outlook.com>
2017-02-09 15:34:05 +0200

    Move DateTime calculation to GigFormViewModel.

--- AcquireFSD.Web/Controllers/GigsController.cs
+++ AcquireFSD.Web/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using AcquireFSD.Web.Models;
 using AcquireFSD.Web.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,14 +16,32 @@ namespace AcquireFSD.Web.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel()
             {
                 Genres = _context.Genres.ToList()
             };
-
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig()
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.DateTime,//DateTime.Parse(($"{viewModel.Date} {viewModel.Time}")),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
