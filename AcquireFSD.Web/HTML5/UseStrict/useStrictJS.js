3a54825b37ed5ff5fde6c65db02cc32faf874cee
dmitry.blinkov <dmitry.blinkov@DMITRYBLINKOV>
2017-04-25 11:45:54 +0300

    Javascript usefull stuff.

---
+++ AcquireFSD.Web/HTML5/UseStrict/useStrictJS.js
@@ -0,0 +1,8 @@
+﻿var agfo = window.agfo || {};
+agfo.documentsTreeWebPart = agfo.documentsTreeWebPart || {};
+"use strict"
+agfo.documentsTreeWebPart.prepareTreeAndShow = function () {
+    init = function () {
+        console.log("Hello from init!")
+    }();
+}();
