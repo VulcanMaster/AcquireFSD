56cb8c7b81d75d37254de53212bd7072d054f688
dmitry.blinkov <dmitry.blinkov@DMITRYBLINKOV>
2017-06-14 16:22:19 +0300

    TestExample - most dump ReactJS example. SimpleReactJSExample - dump "Hello World!" example with bundle.

---
+++ AcquireReactJS/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace AcquireReactJS.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult GetMessage()
+        {
+            return View("TestExample");
+        }
+
+
+        public ActionResult SimpleReactJSExample()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AcquireReactJS/Scripts/SimpleComponent.jsx
@@ -0,0 +1,12 @@
+﻿var Component = React.createClass({
+    render: function(){
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+});
+
+React.renderComponent(
+    <Component/>,
+    document.getElementById('myDiv')
+);
